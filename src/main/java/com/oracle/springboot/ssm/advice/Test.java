6eee6b4e470852edc255f1afa6324fbae618e621
zzh <zzh18832007422@163.com>
2021-11-16 09:39:24 +0800

    b1 commit

--- src/main/java/com/oracle/springboot/ssm/advice/Test.java
+++ src/main/java/com/oracle/springboot/ssm/advice/Test.java
@@ -3,5 +3,6 @@ package com.oracle.springboot.ssm.advice;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Git！");
+        System.out.println("Git b1！");
     }
 }
